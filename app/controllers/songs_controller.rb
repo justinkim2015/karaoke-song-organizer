da0a31da3b2190560a905325dbe36074f495f320
justinkim2015 <justinkim2015@gmail.com>
2022-11-10 21:29:25 +0900

    Add styling

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,10 +1,15 @@
 class SongsController < ApplicationController
+  before_action :set_song, only: [:destroy, :edit, :update, :show]
+
   def index
     @songs = Song.all
   end
 
   def new
     @song = Song.new
+  end
+
+  def show
   end
 
   def create
@@ -18,12 +23,9 @@ class SongsController < ApplicationController
   end
 
   def edit
-    @song = Song.find(params[:id])
   end
 
   def update
-    @song = Song.find(params[:id])
-
     if @song.update(song_params)
       redirect_to root_path
     else
@@ -32,8 +34,6 @@ class SongsController < ApplicationController
   end
 
   def destroy
-    @song = Song.find(params[:id])
-
     @song.destroy
     redirect_to root_path, status: :see_other
   end
@@ -42,5 +42,9 @@ class SongsController < ApplicationController
 
   def song_params
     params.require(:song).permit(:artist, :title)
+  end
+
+  def set_song
+    @song = Song.find(params[:id])
   end
 end
